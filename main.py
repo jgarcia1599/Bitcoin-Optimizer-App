60c73e73d802ff5fa8357a52f61854273a99b312
Junior Francisco Garcia Ayala <junior.f.garcia99@gmail.com>
2019-11-21 17:35:54 -0500

    added flask

---
+++ main.py
@@ -0,0 +1,20 @@
+# please zip the directory that contains your Flask app and upload it to NYU Classes.
+from flask import Flask,render_template
+import requests
+from datetime import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_visitor():
+
+    datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    date= datetime.now().strftime('%Y-%m-%d')
+    time=datetime.now().strftime('%H:%M:%S')
+
+    message="The date is {d} and time is {t}".format(d=date,t=time)
+
+    return render_template("index.html",message=message)
+
+
+app.run(host='0.0.0.0',port=5000,debug=True)
---
+++ templates/script2.js
@@ -0,0 +1,6 @@
+console.log("nowaaaay")
+$(document).ready(function () {
+    $('#fadein').fadeIn(2200);
+    // OR $('div.toshow').show(2200);
+    // OR $('div.toshow').slideDown("slow");
+});
